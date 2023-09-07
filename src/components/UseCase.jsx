0aedf2b086a7a551bcf858e4c94e75c9779b3a68
Aneesh Paul <aneesh16117@gmail.com>
2023-09-07 17:41:16 +0530

    Fixed minor mistake

--- src/components/UseCase.jsx
+++ src/components/UseCase.jsx
@@ -11,28 +11,28 @@ const UseCase = () => {
       <div className="flex flex-col gap-6">
         <div className="bg-white rounded-xl shadow-md p-4 flex cursor-pointer hover:scale-105 duration-500">
           <BsArrowRightShort size={25} />
-          <p className="font-satoshi ml-4 font-semibold">
+          <p className="font-satoshi ml-4 font-medium text-gray-700">
             Stay informed on current events and industry news by quickly
             summarizing relevant articles.
           </p>
         </div>
         <div className="bg-white rounded-xl shadow-md p-4 flex cursor-pointer hover:scale-105 duration-500">
           <BsArrowRightShort size={25} />
-          <p className="font-satoshi ml-4 font-semibold">
+          <p className="font-satoshi ml-4 font-medium text-gray-700">
             Boost productivity by focusing on the most important information
             from long-form content.
           </p>
         </div>
         <div className="bg-white rounded-xl shadow-md p-4 flex cursor-pointer hover:scale-105 duration-500">
           <BsArrowRightShort size={25} />
-          <p className="font-satoshi ml-4 font-semibold">
+          <p className="font-satoshi ml-4 font-medium text-gray-700">
             Enhance your reading experience by obtaining concise and informative
             summaries.
           </p>
         </div>
         <div className="bg-white rounded-xl shadow-md p-4 flex cursor-pointer hover:scale-105 duration-500">
           <BsArrowRightShort size={25} />
-          <p className="font-satoshi ml-4 font-semibold">
+          <p className="font-satoshi ml-4 font-medium text-gray-700">
             Simplify research by summarizing multiple articles for better
             comparison and analysis .
           </p>
